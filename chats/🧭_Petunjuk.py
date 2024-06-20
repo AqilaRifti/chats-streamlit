e8c5f156cbdff80cfddaa50e8929c22b71ebb9de
Aqila Hamizan <73378355+AqilaRifti@users.noreply.github.com>
2024-06-20 07:12:01 +0700

    comii

---
+++ chats/🧭_Petunjuk.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Petunjuk",
+    page_icon="🧭",
+)
+
+st.write("# Selamat Datang! 👋")
+
+st.markdown(
+    """
+    Asisten Pelajar Indonesia menyediakan berbagai AI untuk membantu para penggunanya dalam berbagai 
+    urusan yang dihadapi.
+    ### Nama AI dan kegunaannya:
+    - ❓ Milarian Jawaban -> Cari jawaban dengan instan
+    - 👩‍🏫 Siasat Ngajar -> Rancang strategi mengajar untuk guru
+    - 💬 Hiji Ka Hiji -> AI untuk mengobrol dan menemani berbagai hal
+
+    #### **👈 Langsung saja di coba** untuk melihat berbagai kemampuan API!
+"""
+)
